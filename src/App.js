e3b4d83b5ed5a33f09089bfdabcfd739eaf469fa
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-07-06 10:50:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header/Header';
+import './styles.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {AppBar, Toolbar, Typography} from '@material-ui/core'
+const Header =() =>{
+  return(
+    <>
+        <AppBar position="static">
+                <Toolbar>
+                
+                <ul >
+                  <li>Ttest</li>
+                  <li>Ttest</li>
+                  </ul>
+                
+                </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default Header
